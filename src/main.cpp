f09e487ce0a360471dd05585a40a0c71ce6563d6
Josue Contreras <josudaco@hotmail.com>
2018-03-08 01:43:41 -0500

    UltraSonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,45 @@
 #include <Arduino.h>
+//#include <TimerOne.h>
+
+/*
+* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+*
+* Crated by Dejan Nedelkovski,
+* www.HowToMechatronics.com
+*
+*/
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
 }
 
 void loop() {
-  delay(500);
-  Serial.println("JC");
-    // put your main code here, to run repeatedly:
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance= duration*0.034/2;
+
+  // Prints the distance on the Serial Monitor
+  Serial.print(distance);
+  Serial.print("|");
+  // delay(400);
+  //Serial.println(distance);
 }
